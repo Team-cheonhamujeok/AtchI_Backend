e986ac8511388845195652b5e0e81f9b2f14b9b4
13wjdgk <13wjdgk@naver.com>
2023-03-24 23:07:47 +0900

    spring 기초 세팅

---
+++ atchi/atchi/src/main/java/com/example/atchi/AtchiApplication.java
@@ -0,0 +1,13 @@
+package com.example.atchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtchiApplication.class, args);
+	}
+
+}
